2a788bd3b72bd15a6c297c4995a50e53d5c83c7a
Daniel Vegerano <71361062+floatingchalkoutline@users.noreply.github.com>
2021-06-11 11:50:49 -0700

    first commit of i18n-demo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LocaleSwitcher/>
+    <p>{{ $t('main.welcome', {company: 'MyEtherWallet'}) }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LocaleSwitcher from './components/LocaleSwitcher.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LocaleSwitcher
   }
 }
 </script>
